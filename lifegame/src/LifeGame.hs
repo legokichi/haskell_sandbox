0928997386ba850e84ea9e7b6bf4221c21af49c0
legokichi <legokichi@gmail.com>
2016-05-15 13:36:58 +0900

    初めて動いた

--- lifegame/src/LifeGame.hs
+++ lifegame/src/LifeGame.hs
@@ -1,21 +1,25 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE IncoherentInstances #-}
 
 module LifeGame
-    ( LifeGameIO
+    ( LifeGameIO(..)
     , game
     ) where
+
 
 -- reference
 -- http://d.hatena.ne.jp/lamuu/20060825/1156520199
 -- http://d.hatena.ne.jp/nskj77/20070929/1191074772
+
 
 import Control.Monad (join)
 import Control.Monad.Trans (MonadIO(..))
 import Data.Array.IO (newArray, IOUArray, writeArray, readArray, newListArray, getBounds)
 import Data.Array.Unboxed (UArray, array, listArray, bounds, (!))
 import System.Random (getStdRandom, randomR)
+
 
 --      + Type Constructor
 --      |          + Type Parameter
@@ -27,18 +31,21 @@ newtype LifeGameIO a = LifeGameIO {
 --    |                + Type
 --    |                |
       runLifeGameIO :: IO a
---    + Type Class auto deriving to instance method
---    |        + Super Classes (or deriving (Applicative, Monad) )
---    |        |
     } deriving (Functor, Applicative, Monad)
+--    |        |
+--    |        + Super Classes (or deriving (Applicative, Monad) )
+--    + Type Class auto deriving to instance method
 
+-- FlexibleInstances
 -- + LifeGameIO is instance of MonadIO Type Class
 -- |
 instance MonadIO LifeGameIO where
---  + ideal method: liftIO :: IO a -> m a
---  |        + actual method: LifeGameIO :: IO a -> LifeGameIO a
---  |        |
     liftIO = LifeGameIO
+--  |        |
+--  |        + actual method: LifeGameIO :: IO a -> LifeGameIO a
+--  + ideal method: liftIO :: IO a -> m a
+
+
 
 --   + Type Constructor
 --   |      
@@ -47,25 +54,103 @@ instance MonadIO LifeGameIO where
 --   |       |        |          + element type
 --   |       |        |          |
 type Field = IOUArray (Int, Int) Bool
-{--
-instance Show Field where
-    show field = join $ do
---                                   + getBounds :: (MArray a e m, Ix i) => a i e -> m (i, i)
---                                   |
-        ((_, _), (width, height)) <- liftIO $ getBounds field :: IO ((Int, Int), (Int, Int))
-        return $ show (width, height)
---}
+
 newField :: Int -> Int -> LifeGameIO Field
---                      + LifeGameIO :: IO Field -> LifeGameIO Field
---                      |            + newArray :: Ix i => (i, i) -> e -> m (a i e)
+newField width height = LifeGameIO $ newArray ((0, 0), (width-1, height-1)) False
 --                      |            |
-newField width height = LifeGameIO $ newArray ((0, 0), (width, height)) False
+--                      |            + newArray :: Ix i => (i, i) -> e -> m (a i e)
+--                      + LifeGameIO :: IO Field -> LifeGameIO Field
+
+readField :: Field -> (Int, Int) -> LifeGameIO Bool
+readField fld (x, y) = do
+    ((startW, startH), (stopW, stopH)) <- liftIO $ getBounds fld
+    let a = (x - startW) `mod` (stopW - startW + 1)
+    let b = (y - startH) `mod` (stopH - startH + 1)
+    liftIO $ readArray fld (b, a)
+
+writeField :: Field -> (Int, Int) -> Bool -> LifeGameIO ()
+writeField fld (x, y) flag = do
+    ((startW, startH), (stopW, stopH)) <- liftIO $ getBounds fld
+    let a = (x - startW) `mod` (stopW - startW + 1)
+    let b = (y - startH) `mod` (stopH - startH + 1)
+    liftIO $ writeArray fld (b, a) flag
+
+getFieldNeighbourhoods :: Field -> (Int, Int) -> LifeGameIO [Bool]
+getFieldNeighbourhoods fld (x, y) = do
+    ((startW, startH), (stopW, stopH)) <- liftIO $ getBounds fld
+    alive <- readField fld (x, y)
+    let read (ox, oy) = readField fld (ox + x, oy + y)
+    around <- mapM read neighbourhoods
+    return around
+    where
+        neighbourhoods = [ (-1,-1), (0,-1), (1,-1)
+                         , (-1, 0), (0, 0), (1, 0)
+                         , (-1, 1), (0, 1), (1, 1) ]
+
+
+showField :: Field -> LifeGameIO String
+showField fld = do
+    ((startW, startH), (stopW, stopH)) <- liftIO $ getBounds fld
+    b <- mapM (\x -> do
+        a <- mapM (\y -> get fld x y) [startH..stopH]
+        return $ (join a) ++ "\n"
+     ) [startW..stopW]
+    return $ join b
+    where
+        get :: Field -> Int -> Int -> LifeGameIO String
+        get fld x y = do
+            flag <- liftIO $ readArray fld (x, y)
+            LifeGameIO $ return $ if flag then "#" else "_"
+
+putField :: Field -> LifeGameIO ()
+putField fld = do
+    showField fld >>= \str -> liftIO $ putStrLn str
+--  |             |           |        |
+--  |             |           |        + putStrLn :: String -> IO ()
+--  |             |           + liftIO :: IO () -> LifeGameIO ()
+--  |             + (>>=) :: LifeGameIO String -> (String -> LifeGameIO ()) -> LifeGameIO ()
+--  + showField :: Field -> LifeGameIO String
+
+
+updateField :: Field -> Field -> LifeGameIO ()
+updateField curr next = do
+    ((startW, startH), (stopW, stopH)) <- liftIO $ getBounds curr 
+    mapM_ update [(x, y) | x <- [startW..stopW], y <- [startH..stopH]]
+    where
+        update (x, y) = do
+--                   + getFieldNeighbourhoods :: Field -> (Int, Int) -> LifeGameIO [Bool]
+--                   |
+            bools <- getFieldNeighbourhoods curr (x, y)
+            let centor = bools!!4
+            let others = (count bools) - if centor then 1 else 0
+            case others of
+                3 -> writeField next (x, y) True
+                2 -> writeField next (x, y) centor
+                _ -> writeField next (x, y) False
+        count [] = 0
+        count (x:xs) = if x then 1 + count xs else count xs
+
+
 
 --             + Arrays with unboxed elements. Instances of
 --             |      + index type
 --             |      |   + element type
 --             |      |   |
 type Pattern = UArray (Int, Int) Bool
+
+-- TypeSynonymInstances
+-- IncoherentInstances
+instance Show Pattern where
+    show pat = let
+        ((startW, startH), (stopW, stopH)) = bounds pat
+        in
+        (join $ map (\x ->
+            (join $ map (\y ->
+                if pat!(x, y) then "#" else "_"
+            ) [startH..stopH]) ++ "\n"
+        ) [startW..stopW])
+
+
 
 --        + UArray Int Bool
 --        |
@@ -83,112 +168,102 @@ glider = listArray ((0, 0), (2, 2)) [ False, True,  False
 --            |                      + UArray Int Bool
 --            |                      |
 setPattern :: Field -> Int -> Int -> Pattern -> LifeGameIO ()
-setPattern field offsetX offsetY pattern = do
-    let ((_, _), (width, height)) = bounds pattern
-    let offsets = [(x, y) | x <- [1..width], y <- [1..height]] :: [(Int, Int)]
+setPattern fld offsetX offsetY pat = do
+    let ((startW, startH), (stopW, stopH)) = bounds pat
+    let offsets = [(x, y) | x <- [startW..stopW], y <- [startH..stopH]]
 --  mapM_ :: ((Int, Int) -> IO ()) -> [(Int, Int)] -> IO ()
 --  |
     mapM_ put offsets
     where
---                            + writeArray :: IOUArray (Int, Int) Bool -> (Int, Int) -> Bool -> IO ()
---                            |          + IOUArray (Int, Int) Bool
---                            |          |                                + UArray (Int, Int) Bool
+        put (x, y) = liftIO $ writeArray fld (offsetX + x, offsetY + y) $ pat!(x, y)
 --                            |          |                                |
-        put (x, y) = liftIO $ writeArray field (offsetX + x, offsetY + y) $ pattern!(x, y)
-
+--                            |          |                                + UArray (Int, Int) Bool
+--                            |          + IOUArray (Int, Int) Bool
+--                            + writeArray :: IOUArray (Int, Int) Bool -> (Int, Int) -> Bool -> IO ()
 
 
 
 game :: IO ()
---     + runLifeGameIO :: IO ()
---     |
 game = runLifeGameIO $ do
---    + liftIO :: IO a -> LifeGameIO a
---    |        + putStrLn :: String -> IO ()
---    |        |
-      liftIO $ putStrLn $ show glider
---              + newField :: Int -> Int -> LifeGameIO Field
---              |
-      field1 <- newField 100 100
-      field2 <- newField 100 100
---    + setPattern :: Field -> Int -> Int -> Pattern -> LifeGameIO ()
---    |
-      setPattern field1 0 0 glider
-      liftIO $ putStrLn $ show field1
-      return ()
---    mapM_ (\(x,y) -> randField x y field1) points
---    mapM_ (\(x,y) -> createGlider x y field1) [(10,10),(20,20),(30,30)]
-    {-
-    frame <- frameCreate objectNull idAny "Lifegame" rectNull
-        (wxSYSTEM_MENU + wxCAPTION + wxNO_FULL_REPAINT_ON_RESIZE)
-    windowSetClientSize frame (sz 100 100)
-    panel <- panelCreate frame idAny rectNull 0
-    timer <- windowTimerCreate frame
-    timerOnCommand timer (nextGen field1 field2 panel timer)
-    windowOnPaintRaw panel (drawCells field1)
-    windowShow frame
-    timerStart timer 500 False
-    
-    where
-        --タイマーに登録する世代交代処理
-        nextGen now next w t
-        = do mapM_ (\(x,y) -> nextField x y now next)
-    　　　　　　　　　　　　　　　　　　　　　　　　　[(x,y)|x <- [1..98],y <- [1..98]]
-                timerOnCommand t (nextGen next now w t) --世代をスワップして再登録
-                windowRefresh w True
-
-        --全てのセルを描画する
-        drawCells field dc viewRect updateAreas
-        = do mapM_ (\(x,y) ->
-                do alive <- readArray field (x,y)
-                    when alive (drawPoint dc (pt x y) [color := black])) points
--}
+--     |
+--     + runLifeGameIO :: IO ()
+    test
+--  + liftIO :: IO () -> LifeGameIO ()
+--  |        + putStrLn :: String -> IO ()
+--  |        |
+    liftIO $ putStrLn $ show glider
+--            + newField :: Int -> Int -> LifeGameIO Field
+--            |
+    field1 <- newField 10 10
+    field2 <- newField 10 10
+--  + setPattern :: Field -> Int -> Int -> Pattern -> LifeGameIO ()
+--  |
+    setPattern field1 1 1 glider
+    putField field1
+    let step _ = do {
+        updateField field1 field2;
+        putField field2;
+        updateField field2 field1;
+        putField field1;
+    }
+    mapM_ step [1..100]
+    return ()
 
 
-{-
-neighbourhood :: [(Int, Int)]
-neighbourhood = [ (-1,-1), (0,-1), (1,-1)
-                , (-1, 0), (0, 0), (1, 0)
-                , (-1, 1), (0, 1), (1, 1) ]
 
-nextField :: Int ->Int ->IOUArray (Int,Int) Bool ->IOUArray (Int,Int) Bool ->IO()
-nextField x0 y0 now next = do
-    alive <- readArray now (x0,y0)
-    aroundAlive <- mapM (\(x,y) -> readArray now (x0+x,y0+y)) around
-    case (count aroundAlive) of
-        3 -> writeArray next (x0,y0) True
-        2 -> writeArray next (x0,y0) alive
-        _ -> writeArray next (x0,y0) False
-    where
-        count [] = 0
-        count (x:xs) = if x then 1 + count xs else count xs
 
-points = [(x, y)| x <- [0..99], y <- [0..99]]
--}
 
-{-
---                               + IOUArray (Int, Int) Bool
---                               |
-randFieldOneCell :: Int -> Int -> Field -> LifeGameIO ()
-randFieldOneCell x y field = do
+test :: LifeGameIO ()
+test = do
+    liftIO $ print "test start"
+    field1 <- newField 10 10
+    setPattern field1 1 1 glider
+    bools <- getFieldNeighbourhoods field1 (2, 2)
+    let d = [ False, True,  False
+            , False, False, True
+            , True,  True,  True ]
+    liftIO $ print $ map (\(a, b)-> a == b) $ zip bools d
+    let a = [ (0,0)
+            ,       (1,1),      (1,3)
+            , (2,1),      (2,2)
+            ,                   (3,3)
+            ,                         (4,4) ]
+    let b = map (\(a,b)->(a+10, b+10)) a
+    let c = [ False
+            ,       False,      True
+            , True,        False
+            ,                    True
+            ,                         False ]
+--                  + readField :: Field -> (Int, Int) -> LifeGameIO Bool
+--                  |
+    boolsA <- mapM (readField field1) a
+    boolsB <- mapM (readField field1) b
+    --liftIO $ print boolsA
+    --liftIO $ print boolsB
+    liftIO $ print $ all (\a -> a == True) $ map (\(a, b)-> a == b) $ zip boolsA c
+    liftIO $ print $ all (\a -> a == True) $ map (\(a, b)-> a == b) $ zip boolsB c
+    liftIO $ print "test end"
+    return ()
+
+--                  + IOUArray (Int, Int) Bool
+--                  |
+randFieldOneCell :: Field -> (Int, Int) -> LifeGameIO ()
+randFieldOneCell field (x, y)  = do
 --       + randInt :: LifeGameIO Int
 --       |
     r <- randRange (1, 10)
     let flag = r == 1 -- 1/10 probabry
---  + liftIO :: IO () -> LifeGameIO ()
---  |        + writeArray :: IOUArray (Int, Int) Bool -> (Int, Int) -> Bool -> IO ()
---  |        |          + IOUArray (Int, Int) Bool
---  |        |          |            + Bool
---  |        |          |            |
     liftIO $ writeArray field (x, y) flag
--}
+--  |        |          |            |
+--  |        |          |            + Bool
+--  |        |          + IOUArray (Int, Int) Bool
+--  |        + writeArray :: IOUArray (Int, Int) Bool -> (Int, Int) -> Bool -> IO ()
+--  + liftIO :: IO () -> LifeGameIO ()
 
-{-
+
 randRange :: (Int, Int) -> LifeGameIO Int
---                + LifeGameIO :: IO Int -> LifeGameIO Int
---                |            + getStdRandom :: (StdGen -> (Int, StdGen)) -> IO Int
---                |            |              + randomR :: (Int, Int) -> StdGen -> (Int, StdGen)
---                |            |              |
---                |            |              |
 randRange range = LifeGameIO $ getStdRandom $ randomR range
--}
+--                |            |              |
+--                |            |              + randomR :: (Int, Int) -> StdGen -> (Int, StdGen)
+--                |            + getStdRandom :: (StdGen -> (Int, StdGen)) -> IO Int
+--                + LifeGameIO :: IO Int -> LifeGameIO Int
